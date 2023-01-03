cae48a6d1dd7418af9a9a7f6ad1728a6238e721d
Snehitha19 <99762490+Snehitha19@users.noreply.github.com>
2023-01-03 11:43:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit;
 streamlit.title("My Parents New Healthy Diner");
+streamlit.header("What is the menu");
+streamlit.text("Bread and Butter");
+streamlit.text("Idli with sambar");
+streamlit.text("Dosa with chutney");
+streamlit.text("sandwitch");
